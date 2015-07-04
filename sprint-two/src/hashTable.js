9e9a76f2d8eb143fbd004d63b66914d38c8e9897
Eric King <e.king117@gmail.com>
2015-07-04 14:49:23 -0400

    hashtable works, collision handler is very crude

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,15 +4,41 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
+  //run getIndex, returns an index
+  //check whether the key === key @ index location
+  //if it is, overwrite 
+  //if it isn't, find a new index to store
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var storage = this._storage.get(i);
+  if((storage === undefined) || (k in storage)){
+    var obj = {};
+    obj[k] = v;
+    this._storage.set(i, obj);
+
+  }
+  else{
+    var obj = {};
+    obj[k] = v;
+    this._storage.set(i+1, obj);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var obj = this._storage.get(i);
+  if(k in obj){
+    return obj[k];
+  }
+  else{
+    return this._storage.get(i+1)[k];
+  }
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var obj = {};
+  obj[k] = null;
+  delete this._storage.set(i, obj);
 
 };
 
@@ -21,3 +47,4 @@ HashTable.prototype.remove = function(k){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
